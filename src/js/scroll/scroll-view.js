d1b560dd4e50978e55bba67b660e8890268221dd
Thiago Rossener <thiago.rossener@gmail.com>
2019-11-11 20:59:17 -0300

    Refactored scroll view

--- src/js/scroll/scroll-view.js
+++ src/js/scroll/scroll-view.js
@@ -152,74 +152,15 @@ class ScrollView {
     }, 0);
   }
 
-  _buildParallaxScenes() {
-    const globalElements = [];
-    const sceneElements = [];
-
-    const elements = this._container.querySelectorAll('[data-parallax]');
-
-    elements.forEach(element => {
-      const isEnabled = eval(this._helper.getParallaxProperty(element, 'enabled', this._defaults.parallax.enabled));
-
-      if (isEnabled) {
-        const speed = this._helper.getParallaxProperty(element, 'speed', this._defaults.parallax.speed);
-        const momentum = this._helper.getParallaxProperty(element, 'momentum', this._defaults.parallax.momentum);
-        const stagger = this._helper.getParallaxProperty(element, 'stagger', this._defaults.parallax.stagger);
-        const ease = eval(this._helper.getParallaxProperty(element, 'ease', this._defaults.parallax.ease));
-
-        let item = {
-          element: element,
-          speed: speed,
-          momentum: momentum,
-          stagger: stagger,
-          ease: ease
-        };
-
-        const parallaxType = this._helper.getParallaxProperty(element, 'parallax', this._defaults.parallax.type);
-
-        // Global items
-        if (parallaxType === 'global') {
-          globalElements.push(item);
-        }
-        // Scene items
-        else if (parallaxType === 'scene') {
-          const trigger = this._helper.getParallaxProperty(element, 'trigger', this._defaults.parallax.trigger);
-          const duration = this._helper.getParallaxProperty(element, 'duration', this._defaults.parallax.duration);
-          const offset = this._helper.getParallaxProperty(element, 'offset', this._defaults.parallax.offset);
-          const hook = this._helper.getParallaxProperty(element, 'hook', this._defaults.parallax.hook);
-          const indicator = this._helper.getParallaxProperty(element, 'indicator', this._defaults.parallax.indicator);
-
-          item = Object.assign(item, {
-            trigger: trigger,
-            duration: duration,
-            offset: offset,
-            hook: hook,
-            indicator: indicator
-          });
-
-          sceneElements.push(item);
-        }
-      } else {
-        TweenMax.set(element, { clearProps: 'all' });
-      }
-    });
-
-    if (globalElements.length > 0) {
-      this._buildGlobalParallax(globalElements);
-    }
-
-    if (sceneElements.length > 0) {
-      this._buildScenesParallax(sceneElements);
-    }
-  }
-
   _buildScenes() {
     const domScenes = this._container.querySelectorAll('[data-scene]');
 
     domScenes.forEach(domScene => {
+
       const isEnabled = eval(this._helper.getSceneProperty(domScene, 'enabled', this._defaults.scene.enabled));
 
       if (isEnabled) {
+
         const scene = new ScrollScene({
           triggerElement: this._helper.getSceneProperty(domScene, 'trigger', domScene),
           triggerHook: this._helper.getSceneProperty(domScene, 'hook', this._defaults.scene.triggerHook),
@@ -252,13 +193,182 @@ class ScrollView {
     });
   }
 
+  _createAnimation(scene, domScene) {
+    let tween = new TimelineMax().add('start');
+
+    const domElements = domScene.querySelectorAll('[data-animate]');
+
+    domElements.forEach(domElement => {
+
+      const animation = this._helper.getAnimationProperty(domElement, 'animate', this._defaults.animation.name);
+
+      const animationProps = {
+        autoAlpha: {
+          from: this._helper.getAnimationProperty(domElement, 'from-alpha', this._defaults.animation.alpha.from),
+          to: this._helper.getAnimationProperty(domElement, 'to-alpha', this._defaults.animation.alpha.to)
+        },
+        x: {
+          from: this._helper.getAnimationProperty(domElement, 'from-x', this._defaults.animation.x.from),
+          to: this._helper.getAnimationProperty(domElement, 'to-x', this._defaults.animation.x.to)
+        },
+        y: {
+          from: this._helper.getAnimationProperty(domElement, 'from-y', this._defaults.animation.y.from),
+          to: this._helper.getAnimationProperty(domElement, 'to-y', this._defaults.animation.y.to)
+        },
+        xPercent: {
+          from: this._helper.getAnimationProperty(domElement, 'from-x-percent', this._defaults.animation.xPercent.from),
+          to: this._helper.getAnimationProperty(domElement, 'to-x-percent', this._defaults.animation.xPercent.to)
+        },
+        yPercent: {
+          from: this._helper.getAnimationProperty(domElement, 'from-y-percent', this._defaults.animation.yPercent.from),
+          to: this._helper.getAnimationProperty(domElement, 'to-y-percent', this._defaults.animation.yPercent.to)
+        },
+        scale: {
+          from: this._helper.getAnimationProperty(domElement, 'from-scale', this._defaults.animation.scale.from),
+          to: this._helper.getAnimationProperty(domElement, 'to-scale', this._defaults.animation.scale.to)
+        },
+        rotation: {
+          from: this._helper.getAnimationProperty(domElement, 'from-rotation', this._defaults.animation.rotation.from),
+          to: this._helper.getAnimationProperty(domElement, 'to-rotation', this._defaults.animation.rotation.to)
+        },
+        width: {
+          from: this._helper.getAnimationProperty(domElement, 'from-width', this._defaults.animation.width.from),
+          to: this._helper.getAnimationProperty(domElement, 'to-width', this._defaults.animation.width.to)
+        }
+      };
+
+      const extraProps = {
+        ease: eval(this._helper.getAnimationProperty(domElement, 'ease', this._defaults.animation.ease)),
+        repeat: eval(this._helper.getAnimationProperty(domElement, 'repeat', this._defaults.animation.repeat)),
+        yoyo: eval(this._helper.getAnimationProperty(domElement, 'yoyo', this._defaults.animation.yoyo)),
+        delay: eval(this._helper.getAnimationProperty(domElement, 'delay', this._defaults.animation.delay))
+      };
+
+      const duration = this._helper.getAnimationProperty(domElement, 'duration', this._defaults.animation.duration);
+      const position = this._helper.getAnimationProperty(domElement, 'position', this._defaults.animation.position);
+      const stagger = this._helper.getAnimationProperty(domElement, 'stagger', this._defaults.animation.stagger);
+      const label = this._helper.getAnimationProperty(domElement, 'label', this._defaults.animation.label);
+      const transition = this._helper.getAnimationProperty(domElement, 'transition', this._defaults.animation.transition);
+
+      const fromProps = this._buildState('from', animationProps);
+      const toProps = this._buildState('to', animationProps);
+
+      if (!animation) {
+        let hasProperties = fromProps || toProps;
+        if (hasProperties) {
+          if (stagger) {
+            if (fromProps && toProps) {
+              tween.staggerFromTo(domElement.children, duration, fromProps, Object.assign(toProps, extraProps), stagger, position);
+            }
+            else if (fromProps) {
+              tween.staggerFrom(domElement.children, duration, Object.assign(fromProps, extraProps), stagger, position);
+            }
+            else {
+              tween.staggerTo(domElement.children, duration, Object.assign(toProps, extraProps), stagger, position);
+            }
+            this._domElements.push(domElement.children);
+          } else {
+            if (fromProps && toProps) {
+              tween.fromTo(domElement, duration, fromProps, Object.assign(toProps, extraProps), position);
+            }
+            else if (fromProps) {
+              tween.from(domElement, duration, Object.assign(fromProps, extraProps), position);
+            }
+            else {
+              tween.to(domElement, duration, Object.assign(toProps, extraProps), position);
+            }
+            this._domElements.push(domElement);
+          }
+        }
+      }
+
+      if (label) {
+        tween.add(label);
+      }
+
+      if (transition) {
+        domElement.style.transition = transition;
+      }
+    });
+
+    scene.setTween(tween);
+
+    this._tweens.push(tween);
+  }
+
+  _createCustomAnimation(scene, domScene, sceneName) {
+    // TODO: Refactor this. Custom scenes must be implemented outside ScrollView and registered here
+  }
+
+  _buildParallaxScenes() {
+    const globalItems = [];
+    const sceneItems = [];
+
+    const domElements = this._container.querySelectorAll('[data-parallax]');
+
+    domElements.forEach(domElement => {
+
+      const isEnabled = eval(this._helper.getParallaxProperty(domElement, 'enabled', this._defaults.parallax.enabled));
+
+      if (isEnabled) {
+        const speed = this._helper.getParallaxProperty(domElement, 'speed', this._defaults.parallax.speed);
+        const momentum = this._helper.getParallaxProperty(domElement, 'momentum', this._defaults.parallax.momentum);
+        const stagger = this._helper.getParallaxProperty(domElement, 'stagger', this._defaults.parallax.stagger);
+        const ease = eval(this._helper.getParallaxProperty(domElement, 'ease', this._defaults.parallax.ease));
+
+        let item = {
+          domElement: domElement,
+          speed: speed,
+          momentum: momentum,
+          stagger: stagger,
+          ease: ease
+        };
+
+        const parallaxType = this._helper.getParallaxProperty(domElement, 'parallax', this._defaults.parallax.type);
+
+        // Global items
+        if (parallaxType === 'global') {
+          globalItems.push(item);
+        }
+        // Scene items
+        else if (parallaxType === 'scene') {
+          const trigger = this._helper.getParallaxProperty(domElement, 'trigger', this._defaults.parallax.trigger);
+          const duration = this._helper.getParallaxProperty(domElement, 'duration', this._defaults.parallax.duration);
+          const offset = this._helper.getParallaxProperty(domElement, 'offset', this._defaults.parallax.offset);
+          const hook = this._helper.getParallaxProperty(domElement, 'hook', this._defaults.parallax.hook);
+          const indicator = this._helper.getParallaxProperty(domElement, 'indicator', this._defaults.parallax.indicator);
+
+          item = Object.assign(item, {
+            trigger: trigger,
+            duration: duration,
+            offset: offset,
+            hook: hook,
+            indicator: indicator
+          });
+
+          sceneItems.push(item);
+        }
+      } else {
+        TweenMax.set(domElement, { clearProps: 'all' });
+      }
+    });
+
+    if (globalItems.length > 0) {
+      this._buildGlobalParallax(globalItems);
+    }
+
+    if (sceneItems.length > 0) {
+      this._buildScenesParallax(sceneItems);
+    }
+  }
+
   _buildGlobalParallax(items) {
     const scene = new ScrollScene({
       triggerElement: this._content,
       triggerHook: 'onLeave',
       duration: this._content.offsetHeight
     })
-    .on('update', () => this._updateItems(items, this._controller.getScrollPos()));
+    .on('update', () => this._updateParallaxItems(items, this._controller.getScrollPos()));
 
     this._scenes.push(scene);
     this._controller.addScene(scene);
@@ -280,7 +390,7 @@ class ScrollView {
 
           const delta = this._controller.getScrollPos() - startPos;
 
-          this._updateItems([item], delta);
+          this._updateParallaxItems([item], delta);
         }
       })
       .on('enter', () => during = true)
@@ -292,6 +402,28 @@ class ScrollView {
   
       this._scenes.push(scene);
       this._controller.addScene(scene);
+    });
+  }
+
+  _updateParallaxItems(items, offsetY) {
+    items.forEach(item => {
+      if (item.stagger) {
+        TweenMax.staggerTo(item.domElement.children, item.momentum, {
+          y: offsetY/item.speed,
+          ease: item.ease
+        }, item.stagger);
+      } else {
+        if (item.momentum > 0) {
+          TweenMax.to(item.domElement, item.momentum, {
+            y: offsetY/item.speed,
+            ease: item.ease
+          });
+        } else {
+          TweenMax.set(item.domElement, {
+            y: offsetY/item.speed
+          });
+        }
+      }
     });
   }
 
@@ -310,133 +442,6 @@ class ScrollView {
     this._tweens = [];
     this._scenes = [];
     this._domElements = [];
-  }
-
-  _updateItems(items, offsetY) {
-    items.forEach(item => {
-      if (item.stagger) {
-        TweenMax.staggerTo(item.element.children, item.momentum, {
-          y: offsetY/item.speed,
-          ease: item.ease
-        }, item.stagger);
-      } else {
-        if (item.momentum > 0) {
-          TweenMax.to(item.element, item.momentum, {
-            y: offsetY/item.speed,
-            ease: item.ease
-          });
-        } else {
-          TweenMax.set(item.element, {
-            y: offsetY/item.speed
-          });
-        }
-      }
-    });
-  }
-
-  _createAnimation(scene, domScene) {
-    let tween = new TimelineMax().add('start');
-
-    const items = domScene.querySelectorAll('[data-animate]');
-    items.forEach(item => {
-      const animation = this._helper.getAnimationProperty(item, 'animate', this._defaults.animation.name);
-
-      const animationProps = {
-        autoAlpha: {
-          from: this._helper.getAnimationProperty(item, 'from-alpha', this._defaults.animation.alpha.from),
-          to: this._helper.getAnimationProperty(item, 'to-alpha', this._defaults.animation.alpha.to)
-        },
-        x: {
-          from: this._helper.getAnimationProperty(item, 'from-x', this._defaults.animation.x.from),
-          to: this._helper.getAnimationProperty(item, 'to-x', this._defaults.animation.x.to)
-        },
-        y: {
-          from: this._helper.getAnimationProperty(item, 'from-y', this._defaults.animation.y.from),
-          to: this._helper.getAnimationProperty(item, 'to-y', this._defaults.animation.y.to)
-        },
-        xPercent: {
-          from: this._helper.getAnimationProperty(item, 'from-x-percent', this._defaults.animation.xPercent.from),
-          to: this._helper.getAnimationProperty(item, 'to-x-percent', this._defaults.animation.xPercent.to)
-        },
-        yPercent: {
-          from: this._helper.getAnimationProperty(item, 'from-y-percent', this._defaults.animation.yPercent.from),
-          to: this._helper.getAnimationProperty(item, 'to-y-percent', this._defaults.animation.yPercent.to)
-        },
-        scale: {
-          from: this._helper.getAnimationProperty(item, 'from-scale', this._defaults.animation.scale.from),
-          to: this._helper.getAnimationProperty(item, 'to-scale', this._defaults.animation.scale.to)
-        },
-        rotation: {
-          from: this._helper.getAnimationProperty(item, 'from-rotation', this._defaults.animation.rotation.from),
-          to: this._helper.getAnimationProperty(item, 'to-rotation', this._defaults.animation.rotation.to)
-        },
-        width: {
-          from: this._helper.getAnimationProperty(item, 'from-width', this._defaults.animation.width.from),
-          to: this._helper.getAnimationProperty(item, 'to-width', this._defaults.animation.width.to)
-        }
-      };
-
-      const extraProps = {
-        ease: eval(this._helper.getAnimationProperty(item, 'ease', this._defaults.animation.ease)),
-        repeat: eval(this._helper.getAnimationProperty(item, 'repeat', this._defaults.animation.repeat)),
-        yoyo: eval(this._helper.getAnimationProperty(item, 'yoyo', this._defaults.animation.yoyo)),
-        delay: eval(this._helper.getAnimationProperty(item, 'delay', this._defaults.animation.delay))
-      };
-
-      const duration = this._helper.getAnimationProperty(item, 'duration', this._defaults.animation.duration);
-      const position = this._helper.getAnimationProperty(item, 'position', this._defaults.animation.position);
-      const stagger = this._helper.getAnimationProperty(item, 'stagger', this._defaults.animation.stagger);
-      const label = this._helper.getAnimationProperty(item, 'label', this._defaults.animation.label);
-      const transition = this._helper.getAnimationProperty(item, 'transition', this._defaults.animation.transition);
-
-      const fromProps = this._buildState('from', animationProps);
-      const toProps = this._buildState('to', animationProps);
-
-      if (!animation) {
-        let hasProperties = fromProps || toProps;
-        if (hasProperties) {
-          if (stagger) {
-            if (fromProps && toProps) {
-              tween.staggerFromTo(item.children, duration, fromProps, Object.assign(toProps, extraProps), stagger, position);
-            }
-            else if (fromProps) {
-              tween.staggerFrom(item.children, duration, Object.assign(fromProps, extraProps), stagger, position);
-            }
-            else {
-              tween.staggerTo(item.children, duration, Object.assign(toProps, extraProps), stagger, position);
-            }
-            this._domElements.push(item.children);
-          } else {
-            if (fromProps && toProps) {
-              tween.fromTo(item, duration, fromProps, Object.assign(toProps, extraProps), position);
-            }
-            else if (fromProps) {
-              tween.from(item, duration, Object.assign(fromProps, extraProps), position);
-            }
-            else {
-              tween.to(item, duration, Object.assign(toProps, extraProps), position);
-            }
-            this._domElements.push(item);
-          }
-        }
-      }
-
-      if (label) {
-        tween.add(label);
-      }
-
-      if (transition) {
-        item.style.transition = transition;
-      }
-    });
-
-    scene.setTween(tween);
-
-    this._tweens.push(tween);
-  }
-
-  _createCustomAnimation(scene, domScene, sceneName) {
-    // Implement custom animations here
   }
 
   _buildState(stateKey, properties) {
