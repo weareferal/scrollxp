54af9c221f2016409f3f7a741c6ae990ccbd10dd
Thiago Rossener <thiago.rossener@gmail.com>
2020-11-18 20:30:52 -0300

    Fixed responsive named animations

--- tests/animation.parser.spec.ts
+++ tests/animation.parser.spec.ts
@@ -13,6 +13,30 @@ describe(`Animation parser "name" test`, () => {
   })
 })
 
+describe(`Animation parser responsive "name" test`, () => {
+  it("Should return correct name", () => {
+    const innerWidth = global.window.innerWidth
+
+    const el = document.createElement("div")
+    el.setAttribute("data-animate", "")
+    el.setAttribute("data-lg-animate", "my-name")
+
+    Object.defineProperty(window, "innerWidth", {
+      writable: true,
+      value: 1600,
+    })
+
+    const parser = new Parser().create(AnimationParser)
+    const descriptor = parser.parse(el)
+    expect("my-name").to.equal(descriptor.name)
+
+    Object.defineProperty(window, "innerWidth", {
+      writable: true,
+      value: innerWidth,
+    })
+  })
+})
+
 describe(`Animation parser "duration" test`, () => {
   it("Should return correct duration", () => {
     const el = document.createElement("div")
